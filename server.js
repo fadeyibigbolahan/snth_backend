23f93e481e4b5d2e3bc2258c751a6c738d809d1c
fadeyibi gbolahan <fadeyibi26@gmail.com>
2025-07-10 16:12:50 +0100

    allow origin

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://fadeyibigbolahan.github.io",
+  "https://snthinternational.com",
 ];
 
 app.use(
